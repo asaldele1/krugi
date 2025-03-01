9c3c44241ecddffa160745ce0c149770db135add
asalde_le1 <57564314+asaldele1@users.noreply.github.com>
2025-03-01 20:23:11 +0100

    Add random colour generation and UI import from python class

--- main.py
+++ main.py
@@ -3,29 +3,33 @@ import random
 from typing import Optional
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
 from PyQt6.QtGui import QPainter, QColor, QPaintEvent
-from PyQt6 import uic
+
+from ui_main import Ui_MainWindow
 
 
-class MainWindow(QMainWindow):
+class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi("UI.ui", self)
+        self.setupUi(self)
         self.button = self.findChild(QPushButton, "pushButton")
         self.button.clicked.connect(self.add_circle)
-        self.circles: list[tuple[int, int, int]] = []
+        self.circles: list[tuple[int, int, int, QColor]] = []
 
     def add_circle(self):
         x = random.randint(0, self.width())
         y = random.randint(0, self.height())
         diameter = random.randint(20, 100)
-        self.circles.append((x, y, diameter))
+        color = QColor(random.randint(0, 255),
+                       random.randint(0, 255),
+                       random.randint(0, 255))
+        self.circles.append((x, y, diameter, color))
         self.update()
 
     def paintEvent(self, a0: Optional[QPaintEvent]):
         painter = QPainter(self)
-        painter.setBrush(QColor("yellow"))
-        painter.setPen(QColor("yellow"))
-        for x, y, diameter in self.circles:
+        for x, y, diameter, color in self.circles:
+            painter.setBrush(color)
+            painter.setPen(color)
             painter.drawEllipse(x, y, diameter, diameter)
 
 
